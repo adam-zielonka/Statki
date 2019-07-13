a795e9639de500b605047974d2e039b1e7f638f9
Adam Zielonka <root@adamzielonka.pro>
2019-07-13 11:48:17 +0200

    Create board

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
+import cx from 'classnames'
+import { numberToChar } from './utils'
+
+function Board() {
+  const board = []
+  const firstRow = []
+  for (let j = 0; j < 11; j++) firstRow.push(<Box key={'-1'+j} value={j} />)
+  board.push(<div key={-1} className='row'>{firstRow}</div>)
+
+  for (let i = 0; i < 10; i++) {
+    const row = [<Box key={i} value={numberToChar(i+1)} />]
+    for (let j = 0; j < 10; j++) row.push(<Box key={''+i+j} x={i} j={j} />)
+    board.push(<div key={i} className='row'>{row}</div>)
+  }
+  
+  return <div className='board'>
+    {board}
+  </div>
+}
+
+function Box({value}) {
+  const [state, setState] = useState(0)
+
+  return <input 
+    type='button'
+    disabled={value !== undefined || state%4 === 3}
+    className={cx('box', {red: state%4 === 1, green: state%4 === 2 })}
+    onClick={() => setState(state+1)}
+    value={value ? value : ''}
+  />
+}
 
 function App() {
   return <div className="App">
-      Statki
+    <Board/>
   </div>
 }
 
---
+++ src/utils.js
@@ -0,0 +1,15 @@
+export function numberToChar(number) {
+  switch (number) {
+  case 1: return 'A'
+  case 2: return 'B'
+  case 3: return 'C'
+  case 4: return 'D'
+  case 5: return 'E'
+  case 6: return 'F'
+  case 7: return 'G'
+  case 8: return 'H'
+  case 9: return 'I'
+  case 10: return 'J'
+  default: return ''
+  }
+}
