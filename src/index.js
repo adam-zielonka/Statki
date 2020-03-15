6900d72dd95d2314c41d20336435243387d5d15d
Adam Zielonka <root@adamzielonka.pro>
2020-03-15 17:01:30 +0100

    Start journey to the beginning

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.styl'
-import App from './App'
-import * as serviceWorker from './serviceWorker'
-
-ReactDOM.render(<App />, document.getElementById('root'))
-
-serviceWorker.unregister()
+document.getElementById('board').innerHTML = 'board'
